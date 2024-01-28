385284938feae9a48337a7a92c74567371726133
Vamonosraza <martinezjessy393@gmail.com>
2024-01-28 11:31:47 -0800

    Added navbar

--- portfolio-website/src/app/components/HeroSection.jsx
+++ portfolio-website/src/app/components/HeroSection.jsx
@@ -1,27 +1,47 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
     return (
         <section>
-            <div className="grid grid-cols-1 lg:grid-cols-12">
-                <div className="col-span-7">
-                    <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            Hello, I'm Jessy{" "}
+            <div className="grid grid-cols-1 sm:grid-cols-12">
+                <div className="col-span-7 place-self-center text-center sm:text-left">
+                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600">Hello, I'm {""}
+                        </span> 
+                        <br></br>
+                        <TypeAnimation
+                        sequence={[
+                            // Same substring at the start will only be typed out once, initially
+                            'Jessy',
+                            1000, // wait 1s before replacing "Mice" with "Hamsters"
+                            'Web Developer',
+                            1000,
+                            'Mobile Developer',
+                            1000,
+                            'UX/UI Designer',
+                            1000
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        repeat={Infinity}
+                        />
                     </h1>
 
-                    <p className="text-[#ADB7Be] text-lg mb-6 lg:text">
+                    <p className="text-[#ADB7Be] text-base sm:text-lg mb-6 lg:text-xl">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem
             doloribus non tempora architecto minima quibusdam officiis dolores
             suscipit! Eius doloribus rerum quo reprehenderit. Facere commodi
             adipisci blanditiis eum minus nihil!
                     </p>
                     <div>
-                        <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+                        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 bg-white hover:bg-slate-200 text-white">
                             Hire Me!
                         </button>
-                        <button className="px-6 py-3 rounded-full mr-4 bg-transparent hover:bg-slate-800 text-white border border-color-white ">
-                            Download CV
+                        <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+                            <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2"> Download CV</span>
                         </button>
                     </div>
                 </div>
--- portfolio-website/src/app/page.js
+++ portfolio-website/src/app/page.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import { Container } from "postcss";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] conatiner mx-auto px-12 py-4">
-      <HeroSection/>
+    <main className="flex min-h-screen flex-col bg-[#121212] ">
+        <Navbar/>
+        <div class="container mt-24 mx-auto">
+          <HeroSection/>
+        </div>
     </main>
   );
 }
