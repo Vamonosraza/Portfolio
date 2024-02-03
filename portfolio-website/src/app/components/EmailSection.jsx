fce395cd9d827b6b9dfc912379036885e92b3966
Vamonosraza <martinezjessy393@gmail.com>
2024-02-03 12:35:05 -0800

    Working on email section

---
+++ portfolio-website/src/app/components/EmailSection.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import GithubIcon from
+
+const EmailSection = () => {
+    return (
+        <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4'>
+            <div>
+                <h5 className='text-xl font-bold text-white my-2'>
+                    Let's Connect!
+                </h5>
+                <p className='text-[#ADB7BE] mb-4 max-w-wb'>
+                    Thank you fo rreaching out. I'm always open to new opportunities and meeting new people. Feel free to reach out to me!
+                </p>
+                <div className='socials flex flex-row gap-2'>
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default EmailSection
