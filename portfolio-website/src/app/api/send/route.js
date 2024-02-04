17db119b24a4c2bfa75afae3e5b65a0fcbf23b48
Vamonosraza <martinezjessy393@gmail.com>
2024-02-03 23:58:13 -0800

    Got stuck. Getting error with Postman re email

--- portfolio-website/src/app/api/send/route.js
+++ portfolio-website/src/app/api/send/route.js
@@ -1,19 +1,22 @@
 // import { EmailTemplate } from '../../../components/EmailTemplate';
+import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
+
 const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST() {
     try {
         const data = await resend.emails.send({
-        from: 'Jessy <martinezjessy393@gmail.com>',
-        to: ['martinezjessy393@gmail.com'],
+        from: fromEmail,
+        to: 'jessy@gmail.com',
         subject: 'Hello world',
         react: <><p>Email Body</p></>,
         });
 
-        return Response.json(data);
+        return NextResponse.json(data);
     } catch (error) {
-        return Response.json({ error });
+        return NextResponse.json({ error });
     }
 }
