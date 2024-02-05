32cdfedc3eff6c6398a4324d2f31ecb9949032ec
Vamonosraza <martinezjessy393@gmail.com>
2024-02-04 21:57:20 -0800

    email section works

--- portfolio-website/src/app/api/send/route.js
+++ portfolio-website/src/app/api/send/route.js
@@ -2,28 +2,28 @@
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
+const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
-    const resend = new Resend(process.env.RESEND_API_KEY);
-    const fromEmail = process.env.FROM_EMAIL; 
-    
-    const { body} = await req;
-    const { email, subject, message } = body;
-
+    const { email, subject, message } = await req.json();
+    console.log(email, subject, message);
     try {
         const data = await resend.emails.send({
         from: fromEmail,
-        to: email,
+        to: [fromEmail, email],
         subject: subject,
-        react: <>
-        <h1>{subject}</h1>
-        <p>Thank you for reaching out!</p>
-        <p>New message submit</p>
-        <p>{message}</p></>,
+        react: (
+            <>
+            <h1>{subject}</h1>
+            <p>Thank you for contacting us!</p>
+            <p>New message submitted:</p>
+            <p>{message}</p>
+            </>
+        ),
         });
-
-        return NextResponse.json(data);
+    return NextResponse.json(data);
     } catch (error) {
-        return NextResponse.json({ error });
+    return NextResponse.json({ error });
     }
 }
