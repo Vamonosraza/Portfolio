cc3154483e513494d2a496caf70d1478a98f468a
Vamonosraza <martinezjessy393@gmail.com>
2024-02-04 21:14:34 -0800

    Stuck again with the email

--- portfolio-website/src/app/api/send/route.js
+++ portfolio-website/src/app/api/send/route.js
@@ -3,15 +3,23 @@ import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 
-export async function POST() {
+export async function POST(req, res) {
     const resend = new Resend(process.env.RESEND_API_KEY);
-    const fromEmail = process.env.FROM_EMAIL;   
+    const fromEmail = process.env.FROM_EMAIL; 
+    
+    const { body} = await req;
+    const { email, subject, message } = body;
+
     try {
         const data = await resend.emails.send({
         from: fromEmail,
-        to: `martinezjessy393@gmail.com`,
-        subject: 'Hello world',
-        react: <><p>Email Body</p></>,
+        to: email,
+        subject: subject,
+        react: <>
+        <h1>{subject}</h1>
+        <p>Thank you for reaching out!</p>
+        <p>New message submit</p>
+        <p>{message}</p></>,
         });
 
         return NextResponse.json(data);
--- portfolio-website/src/app/components/EmailSection.jsx
+++ portfolio-website/src/app/components/EmailSection.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import GithubIcon from '../../../public/github-mark.png'
 import LinkedInIcon from '../../../public/linkedIn.webp'
 import Link from 'next/link'
 import Image from 'next/image'
 
 const EmailSection = () => {
+    const [emailSubmitted, setEmailSubmitted] = useState(false)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            email: e.target.email.value,
+            subject: e.target.subject.value,
+            message: e.target.message.value
+        }
+        const JSONdata = JSON.stringify(data);
+        const endpoint = '/api/send';
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSONdata
+        }
+
+        const response = await fetch(endpoint, options);
+        const resData = await response.json();
+        
+
+        if (response.status === 200) {
+            alert('Email sent')
+            setEmailSubmitted(true)
+        }
+
+    }
+
     return (
         <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4'>
             <div>
@@ -24,11 +56,12 @@ const EmailSection = () => {
                 </div>
             </div>
             <div>
-                <form className='flex flex-col'>
+                <form className='flex flex-col' onSubmit={handleSubmit}>
                     <label htmlFor='email' className='text-white block text-small font-medium'>
                         Your email
                     </label>
                     <input 
+                    name='email'
                     type='email' 
                     id='email'
                     required
@@ -37,6 +70,7 @@ const EmailSection = () => {
                         Subject
                     </label>
                     <input 
+                    name='subject'
                     type='text' 
                     id='subject'
                     required
@@ -50,9 +84,13 @@ const EmailSection = () => {
                     required
                     placeholder='Type your message here' className='bg-[#181818] border-[#ADB7BE] border-2 rounded-md py-2 px-4 text-white mb-3' />
                     
-                    <button className='bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 text-white py-2 rounded-md'>
+                    <button type='submit' className='bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 text-white py-2 rounded-md'>
                         Send
                     </button>
+
+                    {
+                        emailSubmitted && ( <p className='text-white text-sm mt-4'>Email sent successfully</p>)
+                    }
                 </form>
             </div>
         </section>
