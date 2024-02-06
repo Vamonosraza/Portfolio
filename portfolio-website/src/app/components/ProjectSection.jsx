60da010d6dc7df40da80ae8a0a438096cde3e4f6
Vamonosraza <martinezjessy393@gmail.com>
2024-02-05 22:21:09 -0800

    Got the links to work

--- portfolio-website/src/app/components/ProjectSection.jsx
+++ portfolio-website/src/app/components/ProjectSection.jsx
@@ -53,7 +53,7 @@ const cardVariants = {
 }
 
     return (
-        <section ref={ref}>
+        <section ref={ref} id='projects'>
         <h2 className='text-center text-4xl font-bold text-white mt-4'>My Projects</h2>
         <div className='text-white flex flex-row justify-center items-center gap-2 py-6 '>
             <ProjectTag 
