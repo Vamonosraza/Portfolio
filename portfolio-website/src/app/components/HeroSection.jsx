5586143b50e666ec22ac243fd3a584815104c217
Vamonosraza <martinezjessy393@gmail.com>
2024-02-09 23:24:49 -0800

    Minor change in pic

--- portfolio-website/src/app/components/HeroSection.jsx
+++ portfolio-website/src/app/components/HeroSection.jsx
@@ -50,7 +50,7 @@ const HeroSection = () => {
                 <div className="col-span-5 place-self-center mt-4 lg:mt-0">
                     <div className="rounded-full bg-[#1818118] w-[250px] h-[250px] relative">
                         <Image
-                        src="/images/selfNobackground.jpeg"
+                        src="/images/watercolorSelf.jpg"
                         alt="selfie"
                         className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
                         width={300}
